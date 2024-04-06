53fa4960d5135c3c2d9568d0834b01d09574923b
Ariane Barguena Tedeschi <72563702+Arianebt@users.noreply.github.com>
2024-04-05 20:34:05 -0400

    More comments

--- models/recipe.js
+++ models/recipe.js
@@ -1,12 +1,15 @@
+// Import Mongoose
 const mongoose = require("mongoose");
 
+// Define recipe schema using Mongoose
 // const recipeSchema = new mongoose.Schema({
 const recipeSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  ingredients: { type: String, required: true },
-  instructions: { type: String, required: true },
-  tags: { type: String },
-  ratings: [{ user: String, rating: Number }],
+  title: { type: String, required: true }, // Define title field with required property
+  ingredients: { type: String, required: true }, // Define ingredients field with required property
+  instructions: { type: String, required: true }, // Define instructions field with required property
+  tags: { type: String }, // Define tags field as optional
+  ratings: [{ user: String, rating: Number }], // Define ratings field as an array of objects with user and rating properties
 });
 
+// Export the recipe schema as a model named 'Recipe' for use in other parts of the application
 module.exports = mongoose.model("Recipe", recipeSchema);
--- models/user.js
+++ models/user.js
@@ -1,11 +1,15 @@
+// Import necessary modules
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
+// Define user schema using Mongoose
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, immutable: true},
   password: { type: String, required: true },
 });
 
+// Plugin passport-local-mongoose to user schema for simplified authentication
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
+// Export the user schema as a model named 'User' for use in other parts of the application
 module.exports = mongoose.model('User', userSchema);
